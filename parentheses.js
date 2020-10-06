abb9235cfebc67983f50fa80247d0e0afd029c47
Olivier Girardot <ogmusik@gmail.com>
2020-10-06 10:05:05 +0200

    headers didn't work trying new headers

--- parentheses.js
+++ parentheses.js
@@ -22,14 +22,14 @@ router.post('/', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Credentials', true);
   const stringCheck = ({
     string: req.body
   });
