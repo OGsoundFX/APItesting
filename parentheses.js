8062ca3626dbaa61d780f5b9375c2db29cace82c
Olivier Girardot <ogmusik@gmail.com>
2020-10-06 10:10:14 +0200

    headers didn't work trying new headers

--- parentheses.js
+++ parentheses.js
@@ -15,18 +15,17 @@ const parenthesesChecker = require('./checker.js');
 const express = require('express'), bodyParser = require('body-parser');
 const router = express.Router();
 
-router.use(bodyParser.json());
 
 router.post('/', (req, res, next) => {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
-
+  
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-
+  
   // Request headers you wish to allow
   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
+  
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   // res.setHeader('Access-Control-Allow-Credentials', true);
@@ -41,11 +40,12 @@ router.post('/', (req, res, next) => {
     res.status(200).json({
       "isValid": false,
       "errorDetails": {
-          "preceedingParentheses": `${parenthesesChecker(stringCheck.string.string).preceeding}`,
-          "erroneosParentheses": `${parenthesesChecker(stringCheck.string.string).erroneos}`
+        "preceedingParentheses": `${parenthesesChecker(stringCheck.string.string).preceeding}`,
+        "erroneosParentheses": `${parenthesesChecker(stringCheck.string.string).erroneos}`
       }
     });
   };
 });
+router.use(bodyParser.json());
 
 module.exports = router;
